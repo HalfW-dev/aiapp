cd40e665f7279f4abdc5b17a5ad86732d3192d2d
Hachida Ahiru <tunghuy2003@gmail.com>
2023-12-21 21:23:54 +0700

    Made the HTTP POST work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState(null);
+    const [response, setResponse] = useState('');
+
+    const handleImageChange = (event) => {
+        setImage(event.target.files[0]);
+    };
+
+    const sendImage = async () => {
+        try {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(image);
+
+            fileReader.onload = async () => {
+                const formData = new FormData();
+                formData.append("file", image);
+
+                const result = await axios.post(
+                    "https://detect.roboflow.com/riichi-mahjong/6",
+                    formData,
+                    {
+                        params: {
+                            api_key: "9zBI3bx7d4qVBh2pX1N1",
+                        },
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                    }
+                );
+
+                setResponse(JSON.stringify(result.data, null, 2));
+            }
+        } catch(error) {
+            console.error('Error sending image:', error.message);
+            console.log('Error response:', error.response.data);
+        }
+    };
+
+    return(
+        <div>
+            <h1>Image Upload and HTTP Request</h1>
+            <input type="file" accept="image/*" onChange={handleImageChange} />
+            <button onClick={sendImage}>Send Image</button>
+                {response && (
+                <div>
+                <h2>Server Response:</h2>
+                <pre>{response}</pre>
+                </div>
+                )}
+        </div>
+    )
 }
 
 export default App;
