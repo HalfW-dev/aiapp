39893a52b8604544344fcff84f165c7d0a9516df
Hachida Ahiru <tunghuy2003@gmail.com>
2023-12-22 10:13:57 +0700

    Result for inference

---
+++ src/components/ImageResult.js
@@ -0,0 +1,16 @@
+function ImageResult({response, status}) {
+    return(
+        <>  {status === 'pending' && (
+                <h2>Working on it...</h2>
+            )}
+            {response && (
+                <div>
+                <h2>Server Response:</h2>
+                <pre>{response}</pre>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default ImageResult;
