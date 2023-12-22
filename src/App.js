63605504f10973205449643f89536f8a42e59a7f
Hachida Ahiru <tunghuy2003@gmail.com>
2023-12-22 10:14:25 +0700

    Unclutter by delegating concerns to other components

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import {useState} from 'react';
-import axios from 'axios';
+import ImageForm from './components/ImageForm';
+import ImageResult from './components/ImageResult';
 
 function App() {
+    const [status, setStatus] = useState('standby'); //standby, pending, done
+
     const [image, setImage] = useState(null);
     const [response, setResponse] = useState('');
-
-    const handleImageChange = (event) => {
-        setImage(event.target.files[0]);
-    };
-
-    const sendImage = async () => {
-        try {
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(image);
-
-            fileReader.onload = async () => {
-                const formData = new FormData();
-                formData.append("file", image);
-
-                const result = await axios.post(
-                    "https://detect.roboflow.com/riichi-mahjong/6",
-                    formData,
-                    {
-                        params: {
-                            api_key: "9zBI3bx7d4qVBh2pX1N1",
-                        },
-                        headers: {
-                            "Content-Type": "multipart/form-data",
-                        },
-                    }
-                );
-
-                setResponse(JSON.stringify(result.data, null, 2));
-            }
-        } catch(error) {
-            console.error('Error sending image:', error.message);
-            console.log('Error response:', error.response.data);
-        }
-    };
-
+    
     return(
-        <div>
-            <h1>Image Upload and HTTP Request</h1>
-            <input type="file" accept="image/*" onChange={handleImageChange} />
-            <button onClick={sendImage}>Send Image</button>
-                {response && (
-                <div>
-                <h2>Server Response:</h2>
-                <pre>{response}</pre>
-                </div>
-                )}
-        </div>
+      <>
+        <h1>Image Upload and HTTP Request</h1>
+        <ImageForm image={image} setImage={setImage} response={response} setResponse={setResponse} setStatus={setStatus}/>
+        <ImageResult response={response} status={status}/>
+      </>
     )
 }
 
